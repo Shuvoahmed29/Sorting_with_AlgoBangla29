68cdcdb11354a21ab075e6c5b2c0f7af75def3da
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-10-19 03:53:06 +0600

    MargeSort_AlgoBangla29.cpp
    Marge Sort by AlgoBangla29

--- MargeSort_AlgoBangla29.cpp
+++ MargeSort_AlgoBangla29.cpp
@@ -50,3 +50,41 @@ int main(){
     cout << arr[i] << " ";
    }
 }
+/*
+    Worst/Best/Average Case time Complexity O(n*log n)
+    Space Complexity O(n)
+    
+    T(n) = T(n/2) + T(n/2)  + n
+           (Left)     (Right)   (Marge)
+
+T(n) = 2T(n/2) + n -------------(1)
+
+Substitutes n/2 in place of n in equ. (1)
+T(n/2) = 2T(n/4) + n/2
+
+Put the value of T(n/2) in equ (1)
+T(n) = 2{ 2T(n/4) + n/2 } + n
+T(n) = 4 T(n/4) + 2n -------------------------------(2)
+
+Substitutes n/4 in place of n in equ. (1)
+T(n/4) = 2T(n/8) + n/4
+
+Put the value of T(n/2) in equ (2)
+T(n) = 4{2T(n/8) + n/4} + 2n
+T(n) = 8T(n/8) + 3n
+T(n) = 2^3 T(n/2^3) + 3n
+Hence,
+T(n) = 2^i T(n/2^i) + i*n ------------------------(3)
+
+Let,
+n/2^i = 1 
+n = 2^i
+log2 (n) = log2 (2^i)
+log2 (n) = i log2 (2)
+i = log2 (n)
+From equ (3),
+T(n) = n T(1) + n*log2 (n)
+T(n) = n + n*log2 (n)
+
+T(n) = O(n*log2 (n))
+*/
