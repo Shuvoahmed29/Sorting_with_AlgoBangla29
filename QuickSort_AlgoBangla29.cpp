d24aeac17a7bf0cabbd66a3c051a3ff26cc370f4
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2023-10-20 01:42:39 +0600

    QuickSort_AlgoBangla29.cpp

--- QuickSort_AlgoBangla29.cpp
+++ QuickSort_AlgoBangla29.cpp
@@ -38,3 +38,67 @@ int main(){
     }
     cout << endl;
 }
+/*
+Best/Average Case Complexity O(n*logn)
+Worst Case Time Complexity O(n^2)
+Space Complexity O(log n)
+
+
+Time Complexity of Quick Sort
+T(n) = T(n-1) + n -----------------------------(1)
+
+Substitute (n-1) in place of n in equ (1)
+T(n-1) = T(n-2) + (n-1)
+
+Put the value of T(n-1) in equ (1)
+T(n) = T(n-2) + (n-1) + n --------------(2)
+
+Substitute (n-2) in place of n in equ (1)
+T(n-2) = T(n-3) + (n-2)
+
+Put the value of T(n-2) in equ (2)
+T(n) =  T(n-3) + (n-2)+ (n-1) + n
+-----------------------------------------
+-----------------------------------------
+T(n) = T(1) + 2 + 3 + .....................................+ (n-2)+ (n-1) + n
+T(n) = 1  + 2 + 3 + .....................................+ (n-2)+ (n-1) + n
+
+T(n) = n(n+1)/2
+T(n) = n^2 / 2 + n/2
+T(n) = O(n^2) [Worst Case]
+
+If the array is divided into two equal part
+T(n) = T(n/2) + T(n/2)  + n
+           (Left)     (Right)   (Marge)
+
+T(n) = 2T(n/2) + n -------------(1)
+
+Substitutes n/2 in place of n in equ. (1)
+T(n/2) = 2T(n/4) + n/2
+
+Put the value of T(n/2) in equ (1)
+T(n) = 2{ 2T(n/4) + n/2 } + n
+T(n) = 4 T(n/4) + 2n -------------------------------(2)
+
+Substitutes n/4 in place of n in equ. (1)
+T(n/4) = 2T(n/8) + n/4
+
+Put the value of T(n/2) in equ (2)
+T(n) = 4{2T(n/8) + n/4} + 2n
+T(n) = 8T(n/8) + 3n
+T(n) = 2^3 T(n/2^3) + 3n
+Hence,
+T(n) = 2^i T(n/2^i) + i*n ------------------------(3)
+
+Let,
+n/2^i = 1 
+n = 2^i
+log2 (n) = log2 (2^i)
+log2 (n) = i log2 (2)
+i = log2 (n)
+From equ (3),
+T(n) = n T(1) + n*log2 (n)
+T(n) = n + n*log2 (n)
+
+T(n) = O(n*log2 (n)) [Best/Average Case]
+*/
